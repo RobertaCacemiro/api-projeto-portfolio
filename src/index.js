1f064de042e31a63b173407071a80a7506711959
Rúbia Cacemiro <100322585+RobertaCacemiro@users.noreply.github.com>
2025-11-12 20:28:07 +0000

    Adicionando metodo POST para cadastro dos projetos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
 const app = express()
+app.use(express.json())
 const port = 3000
-mongoose.connect('mongodb+srv://rubia_mongdb:<db_password>@api-projeto-portfolio.c9l1y92.mongodb.net/?appName=api-projeto-portfolio');
+mongoose.connect('mongodb+srv://rubia_mongdb:bdoxA6PityTdiIC7@api-projeto-portfolio.c9l1y92.mongodb.net/?appName=api-projeto-portfolio');
 
-const Project = mongoose.model()
+const ProjectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  stacks: {
+    type: [String], 
+    default: [] 
+  },
+  image_url: String,
+  access_url: String, 
+  repository_url: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: Boolean,
+    default: true
+  },
+});
+
+const Project = mongoose.model('Project', ProjectSchema);
 
 app.get('/', (req, res) => {
   res.send('Funciona!')
+})
+
+app.post("/cadastro", async (req, res) => {
+  console.log("TESTE", req.body);
+  const project = new Project({
+    title: req.body.title, 
+    description: req.body.description, 
+    stacks: req.body.stacks, 
+    image_url: req.body.image_url, 
+    access_url: req.body.access_url, 
+    repository_url: req.body.repository_url, 
+    status: req.body.status
+})
+
+  await project.save()
+  res.send(project)
 })
 
 app.listen(port, () => {
